9b2e74ac65ae9f84c5254071207e64b693a5800d
Jordan Hobson <hobsoncjordan@gmail.com>
2022-06-23 21:55:39 -0500

    Pair Programming Push

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -1,1 +1,91 @@
 "use strict";
+
+// Array Todos
+
+// TODO 1
+// Creating new array
+let chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+// TODO 2
+// Creating a function to add an element to an array without altering the original array
+function addElementToBeginningOfArray(array,element){
+    let newArray = [element,...array];
+    // Console logging to show that the original array is
+    // untouched and the new array contains the new element
+    console.log(array);
+    console.log(newArray);
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+    // Displaying original array passed in
+    console.log(`Input array: ${array}`);
+    array.unshift(element);
+    // Displaying array after the new element has been added
+    console.log(`Output array: ${array}`);
+}
+
+addElementToBeginningOfArray(chocolateBars,'baby ruth');
+destructivelyAddElementToBeginningOfArray(chocolateBars,'Hersheys')
+
+// TODO 3
+let fastFoodPlaces = ['WhataBurger',"Wendy's","Sonic","Burger King"];
+function addElementToEndOfArray(array,element){
+    let newArray = [...array,element];
+    // Console logging to show that the original array is
+    // untouched and the new array contains the new element
+    console.log(array);
+    console.log(newArray);
+}
+function destructivelyAddElementToEndoOfArray(array,element){
+    // Displaying original array passed in
+    console.log(`Input array: ${array}`);
+    array.push(element);
+    // Displaying array after the new element has been added
+    console.log(`Output array: ${array}`)
+}
+addElementToEndOfArray(fastFoodPlaces,"McDonald's");
+destructivelyAddElementToEndoOfArray(fastFoodPlaces,"Backyard Burger");
+
+//TODO 4
+const years = ['2018','2019','2020','2021','2022'];
+function accessElementInArray(array,index){
+    console.log(array[index]);
+    if (index > array.length-1){
+        alert('The array is not that long.');
+    }
+}
+accessElementInArray(years, 2);
+
+//TODO 5
+function destructivelyRemoveElementFromBeginningOfArray(array){
+    console.log(array);
+    years.pop();
+    console.log(array);
+}
+
+destructivelyRemoveElementFromBeginningOfArray(years);
+
+//TODO 6
+function removeElementFromBeginningOfArray(array){
+    console.log(`Before removed: ${array}`);
+    let testArray = array.slice(1);
+    console.log(`After removed: ${testArray}`);
+}
+
+removeElementFromBeginningOfArray(years);
+
+//TODO 7 (??)
+function destructivelyRemoveElementFromEndOfArray(array){
+    console.log(array);
+    array.pop();
+    console.log(array)
+}
+destructivelyRemoveElementFromEndOfArray(chocolateBars);
+
+//TODO 8
+function removeElementFromEndOfArray(array){
+    let functionArray = array.slice(0, array.length-1);
+    console.log(functionArray);
+}
+removeElementFromEndOfArray(years);
+
+
+
--- loops/loops.js
+++ loops/loops.js
@@ -10,14 +10,34 @@ let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
 let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
 // Example output: 
 // 276 + 351 = 627
+let total1 = 0;
+let total2 = 0;
+function addArrays(array1,array2){
+    for(let i = 0; i<array1.length; i++){
+        total1 = total1 + array1[i];
+    }
+    for(let i = 0; i<array2.length; i++){
+        total2 = total2 + array2[i];
+    }
+    let arraySum = total1 + total2;
+    let finalMessage = `Array 1's total was ${total1}. Array 2's total was ${total2}. The sum of both totals is ${arraySum}.`;
+    console.log(finalMessage);
 
+}
 
+addArrays(arr_1,arr_2);
 // 2) Using a for loop print all even numbers up to and including n. Don’t include 0.
 
 let n1 = 22; 
 // Example output: 
 // 2 4 6 8 10 12 14 16 18 20 22 OR each item on a new line
- 
+
+function allEven(number){
+    for (let i = number; i > 0; i=i-2 ){
+        console.log(i);
+    }
+}
+allEven(22);
 
 
 // 3) Using a for loop output the elements in reverse order
@@ -25,7 +45,10 @@ let n1 = 22;
 let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
 // Example output: 
 // true 3.5  be  false cannot  true 9 what 43 OR each item on a new line
-
+arr.slice().reverse()
+    .forEach(function(item) {
+            console.log(item);
+        });
 
 
 
@@ -37,7 +60,14 @@ let arr_3   = [4, 6, 7];
 let arr_4    = [8, 1, 9];
 // Example output: 
 // [12, 7, 16]
-
+function squishSums(array1, array2){
+    let sumArray = [];
+    for (let i = 0; i<array1.length;i++){
+        sumArray.push(array1[i]+array2[i]);
+    }
+    console.log(sumArray);
+}
+squishSums(arr_3,arr_4);
 
 
 
